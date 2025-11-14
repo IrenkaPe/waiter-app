8af1d6c8f6d066704a1ae0d2063949bb1ee78c73
Klaudia Perszke <klaudia.perszke@gmail.com>
2025-11-14 13:34:01 +0100

    add redux store with initial state and thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 
